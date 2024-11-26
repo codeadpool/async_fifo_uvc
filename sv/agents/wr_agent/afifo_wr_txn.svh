d370c60fe60c7dc066bd5dbc07988812c4436fb5
codeadpool <72756153+codeadpool@users.noreply.github.com>
2024-11-26 13:48:06 -0500

    updated wr_txn

--- sv/agents/wr_agent/afifo_wr_txn.svh
+++ sv/agents/wr_agent/afifo_wr_txn.svh
@@ -6,7 +6,9 @@ class afifo_wr_txn #(int DATA_WIDTH =32) extends uvm_sequence_item;
   rand bit winc;
   rand logic [DATA_WIDTH -1:0] wdata;
 
-  `uvm_object_utils_begin(afifo_wr_txn)
+  // for a parametrized object class we need to 
+  typedef afifo_wr_txn#(DATA_WIDTH) this_t;
+  `uvm_object_param_utils_begin(this_t)
     `uvm_field_int( winc, UVM_ALL_ON)
     `uvm_field_int(wdata, UVM_ALL_ON)
   `uvm_object_utils_end
