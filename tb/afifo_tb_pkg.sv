757b6d4ecaf59dc301a945a53ce770378f1aad01
codeadpool <72756153+codeadpool@users.noreply.github.com>
2025-01-13 15:35:10 -0500

    updated pkgs

---
+++ tb/afifo_tb_pkg.sv
@@ -0,0 +1,9 @@
+package afifo_tb_pkg;
+  import afifo_pkg::*;
+  import afifo_rd_agent_pkg::*;
+  import afifo_wr_agent_pkg::*;
+  import afifo_env_pkg::*;
+  import afifo_seq_pkg::*;
+  import afifo_test_pkg::*;
+  import tb_params_pkg::*;
+endpackage
